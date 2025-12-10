0abc241bd941c0dc27d6dfb596d20544eba2d16b
Mert Milenov <mertimladenov@gmail.com>
2025-12-10 21:32:21 +0000

    Moved the firebase configs to an env file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore/lite";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCHD4Swrly7lGQY9smI5Zi8bjifUEPldE8",
-  authDomain: "connections-bulgaria.firebaseapp.com",
-  projectId: "connections-bulgaria",
-  storageBucket: "connections-bulgaria.firebasestorage.app",
-  messagingSenderId: "956324176782",
-  appId: "1:956324176782:web:3254d9e9d37bad67e8fadf",
-  measurementId: "G-KWT3EX2JYH"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
