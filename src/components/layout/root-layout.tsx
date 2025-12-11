2c0bab45485fa086e118005bb77b363429ac2df8
blagoySimandov <baggy.simandoff@gmail.com>
2025-12-11 00:56:46 +0000

    feat: connect db

--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Navbar } from "./navbar";
+
+const queryClient = new QueryClient();
 
 export function RootLayout() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
       <Outlet />
-    </>
+    </QueryClientProvider>
   );
 }
--- src/pages/game/game-page.tsx
+++ src/pages/game/game-page.tsx
@@ -1,6 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import { ConnectionsGame } from "@/components/game/connections-game";
-import { samplePuzzle } from "@/data";
+import { connectionService } from "@/lib";
 
 export function GamePage() {
-  return <ConnectionsGame puzzle={samplePuzzle} />;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["puzzle", "today"],
+    queryFn: () => connectionService.getPuzzleForCurrentDate(),
+  });
+
+  const puzzle = data?.[0];
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading puzzle: {error.message}</div>;
+  if (!puzzle) return <div>No Puzzle for today :/</div>;
+
+  return <ConnectionsGame puzzle={puzzle} />;
 }
